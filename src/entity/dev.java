668e3ed0285ee99d3a0908eae0d618ec24b61a51
89368 <893685821@qq.com>
2020-09-04 18:48:50 +0800

    测试2

--- src/entity/dev.java
+++ src/entity/dev.java
@@ -1,4 +1,18 @@
 package entity;
 
 public class dev {
+    public dev(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
 }
